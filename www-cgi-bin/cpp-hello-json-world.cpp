cd36a4ce0e6f737c8e55cb86435f0adb6a83a792
asvo <asvo@ucsd.edu>
2026-01-29 12:36:04 -0800

    Fix newline from ctime in cpp hello json

--- www-cgi-bin/cpp-hello-json-world.cpp
+++ www-cgi-bin/cpp-hello-json-world.cpp
@@ -9,14 +9,19 @@ int main() {
 
     time_t timestamp;
     time(&timestamp); 
+    // ctime adds a newline when converted to a string. Strip it.
+    char* t = ctime(&timestamp);
+    if (t[strlen(t)-1] == '\n') {
+        t[strlen(t)-1] = '\0';
+    }
     
     const char* clientIP = getenv("REMOTE_ADDR");
     
     cout << "{"
+         <<     "\"time\":\"" << t << "\","
+         <<     "\"IP\":\"" << clientIP << "\""
+         <<     "\"message\":\"This page was generated with C++ and made by Ashley of Team Ate!\","
          <<     "\"title\":\"Hello, C++!\","
          <<     "\"heading\":\"Hello, C++!\","
-         <<     "\"message\":\"This page was generated with C++ and made by Ashley of Team Ate!\","
-         <<     "\"time\":\"" << ctime(&timestamp) << "\","
-         <<     "\"IP\":\"" << clientIP << "\""
          << "}";
 }
