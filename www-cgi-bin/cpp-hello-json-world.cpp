e8794640615137a1dd04028a04fdc52be00e7580
asvo <asvo@ucsd.edu>
2026-01-29 12:11:03 -0800

    Fix cpp JSON formatting

--- www-cgi-bin/cpp-hello-json-world.cpp
+++ www-cgi-bin/cpp-hello-json-world.cpp
@@ -9,15 +9,14 @@ int main() {
 
     time_t timestamp;
     time(&timestamp); 
-    cout << "<p>This program was generated at: " << ctime(&timestamp) << "</p>";
     
     const char* clientIP = getenv("REMOTE_ADDR");
     
     cout << "{"
-         <<     "\'title\': \'Hello, C++!\'"
-         <<     "\'heading\': \'Hello, C++!\'"
-         <<     "\'message\': \'This page was generated with C++ and made by Ashley of Team Ate!\'"
-         <<     "\'time\': \'" << ctime(&timestamp) << "\'"
-         <<     "\'IP\': \'" << clientIP << "\'"
+         <<     "\"title\":\"Hello, C++!\","
+         <<     "\"heading\":\"Hello, C++!\","
+         <<     "\"message\":\"This page was generated with C++ and made by Ashley of Team Ate!\","
+         <<     "\"time\":\"" << ctime(&timestamp) << "\","
+         <<     "\"IP\":\"" << clientIP << "\""
          << "}";
 }
