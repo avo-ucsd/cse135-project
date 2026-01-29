493579692f971dc9431bd43d39662e4fde783e45
asvo <asvo@ucsd.edu>
2026-01-29 11:41:14 -0800

    Create CPP html-hello-world

---
+++ www-cgi-bin/cpp-hello-html-world.cpp
@@ -0,0 +1,38 @@
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Cache-Control: no-cache\n"
+         << "Content-Type: text/html\n\n";
+
+    cout << "<!DOCTYPE html>"
+         << "<html>"
+         << "<head>"
+         << "<title>Hello CGI World! (C++)</title>"
+         << "<head>";
+
+    cout << "<body>"
+         << "<h1 align=center>Hello HTML World</h1><hr/>"
+         << "<p>Hello World! This page was changed by Ashley of Team Ate!</p>"
+         << "<blockquote><p>"
+         <<     "Why do you want to program in C++?" 
+         << "<p></blockquote>"
+         << "<p>-My teammates</p>"
+         << "<p>This page was generated with the Perl programming langauge</p>";
+         
+    time_t timestamp;
+    time(&timestamp); 
+    cout << "<p>This program was generated at: " << ctime(&timestamp) << "</p>";
+    
+    const char* clientIP = getenv("REMOTE_ADDR");
+    if (clientIP != nullptr) {
+        cout << "<p>Your current IP address is: " << clientIP << "</p>";
+    } else {
+        cout << "<p>There was an error with finding your IP address. :(</p>";
+    }
+
+    cout << "</body>"
+         << "</html>";
+}
