1e2f141f7ee25168afa9d96d5f6f5c19fb55a2ec
asvo <asvo@ucsd.edu>
2026-01-30 17:29:15 -0800

    Remove Claude work on echo form

--- www/hw2/echo-form.js
+++ www/hw2/echo-form.js
@@ -32,11 +32,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
             }
         }
 
-        console.log("=============== ENDPOINT ===============\n", endpoint);
-        console.log("=============== METHOD ===============\n", method);
-        console.log("=============== ENCODING ===============\n", encoding);
-        console.log("=============== BODY ===============\n", body);
-
         fetch(modifiedEndpoint, {
               method: method,
               headers: {
@@ -46,8 +41,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         })
         .then(response => response.text())
         .then(data => {
-            // Display the response somewhere
-            console.log('Response:', data);
             const output = document.querySelector('#echo-output');
             output.innerHTML = data;
         })
@@ -56,99 +49,3 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 });
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const form = document.querySelector('form');
-
-//     form.addEventListener('submit', (e) => {
-//         const method = form.querySelector('select[id="http-method"]').value;
-//         const endpoint = form.querySelector('select[id="endpoint"]').value;
-//         const encoding = form.querySelector('select[id="encoding"]').value;
-
-//         // For GET and POST, let the native form submission handle it
-//         if (method === 'GET' || method === 'POST') {
-//             // Set the form action and method
-//             form.setAttribute('action', endpoint);
-//             form.setAttribute('method', method);
-            
-//             // Set encoding based on selection
-//             if (encoding === 'application/json') {
-//                 // JSON encoding for forms isn't natively supported, 
-//                 // so we need to use fetch even for POST
-//                 e.preventDefault();
-//                 submitWithFetch(endpoint, method, encoding);
-//             } else {
-//                 // Let native form submission happen for x-www-form-urlencoded
-//                 form.setAttribute('enctype', 'application/x-www-form-urlencoded');
-//                 // Don't prevent default - let it navigate
-//             }
-//         } else {
-//             // For PUT and DELETE, we must use fetch
-//             e.preventDefault();
-//             submitWithFetch(endpoint, method, encoding);
-//         }
-//     });
-// });
-
-// /**
-//  * Submit form data using fetch API (for PUT, DELETE, or JSON encoding)
-//  */
-// function submitWithFetch(endpoint, method, encoding) {
-//     const form = document.querySelector('form');
-    
-//     // Build the data object from form inputs (excluding control fields)
-//     const bodyAsObject = {
-//         "name": form.querySelector('input[id="name"]').value,
-//         "favorite-food": form.querySelector('input[id="favorite-food"]').value
-//     };
-
-//     let finalEndpoint = endpoint;
-//     let body = null;
-//     let headers = {};
-
-//     if (method === 'GET') {
-//         // For GET, append data as query parameters
-//         const params = new URLSearchParams(bodyAsObject);
-//         const queryString = params.toString();
-//         if (queryString) {
-//             finalEndpoint = `${endpoint}?${queryString}`;
-//         }
-//     } else {
-//         // For POST, PUT, DELETE - set body and headers based on encoding
-//         if (encoding === 'application/json') {
-//             body = JSON.stringify(bodyAsObject);
-//             headers['Content-Type'] = 'application/json';
-//         } else {
-//             body = new URLSearchParams(bodyAsObject);
-//             headers['Content-Type'] = 'application/x-www-form-urlencoded';
-//         }
-//     }
-
-//     // Make the fetch request
-//     fetch(finalEndpoint, {
-//         method: method,
-//         headers: headers,
-//         body: body
-//     })
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-//         return response.text();
-//     })
-//     .then(data => {
-//         // Open the response in a new window/tab to mimic HTTPBin behavior
-//         const newWindow = window.open('', '_blank');
-//         if (newWindow) {
-//             newWindow.document.write('<pre>' + escapeHtml(data) + '</pre>');
-//             newWindow.document.close();
-//         } else {
-//             // If popup blocked, display inline
-//             displayInline(data);
-//         }
-//     })
-//     .catch(error => {
-//         console.error('Error:', error);
-//         alert(`Error: ${error.message}`);
-//     });
-// }
